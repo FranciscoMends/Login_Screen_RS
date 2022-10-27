d3bb7341199809ab38425eab0c0d1b0b78c40848
Francisco Mendes <fmmagalhaesfilho@aluno.uespi.br>
2022-10-27 15:31:38 -0300

    Add user and one function

--- script's/script.js
+++ script's/script.js
@@ -1,3 +1,15 @@
+function acount_validation() {
+    // var login = window.document.getElementById("user")
+    // var senha = window.document.getElementById("password")
+
+    // user = String(login.value)
+    // password = String(senha.value)
+    
+    // if (user == 'mendes' && password == 'fox123') {
+        redirecting_one()
+    // }
+}
+
 function redirecting_one() {
     window.location.replace("inicial_page.html");
 }
