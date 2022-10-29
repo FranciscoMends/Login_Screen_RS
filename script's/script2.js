9888789614b2bd125ff57a979b7a0b0de1eea848
Francisco Mendes <fmmagalhaesfilho@aluno.uespi.br>
2022-10-29 19:03:45 -0300

    Update script2.js

--- script's/script2.js
+++ script's/script2.js
@@ -3,3 +3,18 @@ function redirecting_back() {
         window.open(window.location.replace("index.html"))
     }
 }
+
+const btn = document.getElementById("btn-top")
+btn.addEventListener("click", function(){
+    window.scrollTo(0,0)
+})
+
+document.addEventListener('scroll', ocult)
+
+function ocult() {
+    if (window.scrollY > 100){
+        btn.style.display = "flex"
+    } else {
+        btn.style.display = "none"
+    }
+}
