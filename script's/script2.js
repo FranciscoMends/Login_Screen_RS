03c0636e5e4e75d1e77b8311156ecdf0bc925bed
Francisco Mendes <fmmagalhaesfilho@aluno.uespi.br>
2022-11-03 16:02:26 -0300

    Update script2.js

--- script's/script2.js
+++ script's/script2.js
@@ -5,14 +5,14 @@ function redirecting_back() {
 }
 
 const btn = document.getElementById("btn-top")
-btn.addEventListener("click", function(){
-    window.scrollTo(0,0)
+btn.addEventListener("click", function () {
+    window.scrollTo(0, 0)
 })
 
 document.addEventListener('scroll', ocult)
 
 function ocult() {
-    if (window.scrollY > 100){
+    if (window.scrollY > 100) {
         btn.style.display = "flex"
     } else {
         btn.style.display = "none"
